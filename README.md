7b565c96f03b136ddc2781ac8c8b8deae62cfc4d
Mary Heintz <mary.heintz@cern.ch>
2018-04-24 12:58:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# google-calendar-api-ruby
+
+Some scripts to grab data from a public google calendar to be displayed on a digital sign.
+
+You will need public google calendars and an API key.  The file config/config-example.yml should be renamed
+to config/config.yml and edited with your calendar ids and API key.  You should not store your config.yml 
+file with your actual API key in a public repository.
---
+++ get_events.rb
@@ -0,0 +1,38 @@
+require 'google/apis/calendar_v3'
+require 'yaml'
+
+#Google::Apis.logger.level = Logger::DEBUG
+
+# Get config info
+config = YAML.load_file("config/config.yml")
+
+calendar = Google::Apis::CalendarV3::CalendarService.new
+calendar.key = config['apikey']
+calendarids = config['calendars']
+
+events = Array.new
+
+calendarids.each do |name, id|
+	entries = calendar.list_events(id,
+		always_include_email: false,
+		time_min: '2018-04-23T00:00:00-05:00',
+	 	time_max: '2018-04-23T23:59:59-05:00'
+	)
+
+	entries.items.each do |item|
+		events << { 
+			summary: "#{item.summary}", 
+			description: "#{item.description}", 
+			start: "#{item.start.date_time}", 
+			location: "#{item.location}"
+		}
+	end
+end
+
+events.each do |event|
+	puts event[:summary]
+	puts event[:description]
+	puts event[:start]
+	puts event[:location]
+	puts "====="
+end
