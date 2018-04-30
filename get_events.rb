11a1b13a3cdee086309ca8207beca6c65db854c8
Mary Heintz <mary.heintz@cern.ch>
2018-04-30 10:49:58 -0500

    Fix dates for when searching

--- get_events.rb
+++ get_events.rb
@@ -14,19 +14,26 @@ calendarids = config['calendars']
 # Get today & tomorrow which is used for the range 
 today = Date.today
 tomorrow = today + 1
+nextweek = today + 8
 start_of_day = today.to_datetime.rfc3339
 end_of_day = start_of_day.gsub("T00:00","T23:59")
 
 events = Array.new
+upcoming = Array.new
 
 calendarids.each do |name, id|
 	entries = calendar.list_events(id,
-		always_include_email: false,
 		single_events: true,
 		time_min: start_of_day,
 	 	time_max: end_of_day
 	)
-	
+
+	 upcoming_entries = calendar.list_events(id,
+		single_events: true,
+		time_min: tomorrow.to_datetime.rfc3339,
+		time_max: nextweek.to_datetime.rfc3339
+	)
+
 	unless entries.items.empty?
 		entries.items.each do |item|
 			events << { 
@@ -37,12 +44,28 @@ calendarids.each do |name, id|
 			}
 		end
 	end
+
+	unless upcoming_entries.items.empty?
+		upcoming_entries.items.each do |item|
+			upcoming << {
+				start: item.start.date_time,
+				summary: item.summary,
+				description: item.description,
+				location: item.location
+			}
+		end
+	end
 end
 
-unless events.empty?
-	sorted = events.sort_by { |x| x[:start] }
+open('today.html', 'w') do |f|
+	if events.empty?
+		f.puts "<div class='todaytitle'>UCHICAGO PHYSICS HISTORY</div>"
+		f.puts "<img src='http://physics-pics.uchicago.edu/images/1248/medium/IMG_0038.jpg' alt='Physicists in Action' class='img-responsive' />"
+		f.puts "<p>If you can identify any people in this picture, please email maryh@hep.uchicago.edu about image #1248.</p>"
+	else
+		sorted = events.sort_by { |x| x[:start] }
 
-	open('today.html', 'w') do |f|
+		f.puts "<div class='todaytitle'>TODAY'S EVENTS</div>"
 		sorted.each do |event|
 			f.puts "<span class='left'>#{event[:start].strftime("%-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
 			f.puts "#{event[:summary]}"
@@ -55,3 +78,22 @@ unless events.empty?
 		end
 	end
 end
+
+open('upcoming.html','w') do |f|
+	sorted = upcoming.sort_by { |x| x[:start] }
+	f.puts "<div class='todaytitle'>UPCOMING EVENTS</div>"
+	sorted.each do |event|
+		f.puts "<div class='timeroom'>"
+		f.puts "<span class='left'>#{event[:start].strftime("%A, %B %-d at %-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
+		f.puts "</div>"
+		f.puts "<div class='summary'>#{event[:summary]}</div>"
+		f.puts "<hr />"
+		# puts event[:summary]
+		# #puts event[:description]
+		# puts event[:start].strftime("%-l:%M %p")
+		# puts event[:location]
+		# puts "====="
+	end
+	f.puts "</div>"
+end
+
