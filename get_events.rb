bf15c163eab6c2f95f6cf6864c64eee65fcbaa16
Mary Heintz <mary.heintz@cern.ch>
2018-06-21 11:19:54 -0500

    Fixed date parsing for utc time zone and added All Day possibility

--- get_events.rb
+++ get_events.rb
@@ -1,8 +1,9 @@
 require 'google/apis/calendar_v3'
 require 'yaml'
 require 'date'
+require 'time'
 
-Google::Apis.logger.level = Logger::DEBUG
+#Google::Apis.logger.level = Logger::DEBUG
 
 # Get config info
 config = YAML.load_file("config/config.yml")
@@ -14,9 +15,11 @@ calendarids = config['calendars']
 # Get range of dates for which we want to get events
 localtime = Time.now.getlocal
 today = localtime.to_date
+tomorrow_date = Time.now.getlocal + 86400 # Have to add seconds when working with time 86400 = 60 secs * 60 min * 24 hours
 last_upcoming_event_date = today + 8
 start_of_day = DateTime.parse(localtime.strftime("%Y-%m-%dT00:00:00%z"))
 end_of_day = DateTime.parse(localtime.strftime("%Y-%m-%dT23:59:00%z"))
+tomorrow = DateTime.parse(tomorrow_date.strftime("%Y-%m-%dT00:00:00%z"))
 last_upcoming_event = DateTime.parse(last_upcoming_event_date.strftime("%Y-%m-%dT23:59:59%z"))
 
 events = Array.new
@@ -31,12 +34,15 @@ calendarids.each do |name, id|
 
 	 upcoming_entries = calendar.list_events(id,
 		single_events: true,
-		time_min: end_of_day.rfc3339,
+		time_min: tomorrow.rfc3339,
 		time_max: last_upcoming_event.rfc3339
 	)
 
 	unless entries.items.empty?
 		entries.items.each do |item|
+			if item.start.date_time.nil?
+				item.start.date_time = DateTime.parse(item.start.date)
+			end
 			events << { 
 				start: item.start.date_time, 
 				summary: item.summary, 
@@ -48,6 +54,9 @@ calendarids.each do |name, id|
 
 	unless upcoming_entries.items.empty?
 		upcoming_entries.items.each do |item|
+			if item.start.date_time.nil?
+				item.start.date_time = DateTime.parse(item.start.date)
+			end
 			upcoming << {
 				start: item.start.date_time,
 				summary: item.summary,
@@ -68,14 +77,15 @@ open('today.html', 'w') do |f|
 
 		f.puts "<div class='todaytitle'>TODAY'S EVENTS</div>"
 		sorted.each do |event|
-			f.puts "<span class='left'>#{event[:start].strftime("%-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
-			f.puts "#{event[:summary]}"
-			f.puts "<hr />"
-			# puts event[:summary]
-			# #puts event[:description]
-			# puts event[:start].strftime("%-l:%M %p")
-			# puts event[:location]
-			# puts "====="
+			if (event[:start].strftime("%-l:%M %p") == "12:00 AM")
+				f.puts "<span class='left'>All Day</span><span class='right'>#{event[:location]}</span>"
+				f.puts "#{event[:summary]}"
+				f.puts "<hr />"
+			else
+				f.puts "<span class='left'>#{event[:start].strftime("%-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
+				f.puts "#{event[:summary]}"
+				f.puts "<hr />"
+			end
 		end
 	end
 end
@@ -85,15 +95,14 @@ open('upcoming.html','w') do |f|
 	f.puts "<div class='todaytitle'>UPCOMING EVENTS</div>"
 	sorted.each do |event|
 		f.puts "<div class='timeroom'>"
-		f.puts "<span class='left'>#{event[:start].strftime("%A, %B %-d at %-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
+		if (event[:start].strftime("%-l:%M %p") == "12:00 AM")
+			f.puts "<span class='left'>#{event[:start].strftime("%A, %B %-d, All Day")}</span><span class='right'>#{event[:location]}</span>"
+		else
+			f.puts "<span class='left'>#{event[:start].strftime("%A, %B %-d at %-l:%M %p")}</span><span class='right'>#{event[:location]}</span>"
+		end
 		f.puts "</div>"
 		f.puts "<div class='summary'>#{event[:summary]}</div>"
 		f.puts "<hr />"
-		# puts event[:summary]
-		# #puts event[:description]
-		# puts event[:start].strftime("%-l:%M %p")
-		# puts event[:location]
-		# puts "====="
 	end
 	f.puts "</div>"
 end
